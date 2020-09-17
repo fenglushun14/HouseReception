5c9c267768720bb6e1ad27f2827db751132ff4ce
fenglushun14 <71258074+fenglushun14@users.noreply.github.com>
2020-09-17 17:10:34 +0800

    添加项目文件。

---
+++ HouseReception/Models/AdministratorData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseReception.Models
+{
+    public class AdministratorData
+    {
+        public int AId { get; set; }
+        public int AName { get; set; }
+        public string APlaneNumber { get; set; }
+        public int Card { get; set; }
+        public int BeginTime { get; set; }
+        public int APicture { get; set; }
+        public int APosition { get; set; }
+        public int CompanyCard { get; set; }
+        public int Company { get; set; }
+        public int Password { get; set; }
+
+    }
+}
